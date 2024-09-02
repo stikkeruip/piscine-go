8b9a87ac9b14bcf16f9906a8adf9ad73875cb961
ustikker <stikkeruipko@gmail.com>
2024-09-02 15:52:37 +0300

    gfd

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 curl -s "https://platform.zone01.gr/assets/superhero/all.json" | \
-jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID | tonumber)) | .connections.relatives' | tr '\n' '; '
+jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID | tonumber)) | .connections.relatives' | awk 'ORS="; "' | sed 's/; $//'
 
