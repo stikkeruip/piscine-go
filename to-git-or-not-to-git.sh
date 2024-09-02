b6d0773283816da3430af04c1fb46b0cea1b6c36
ustikker <stikkeruipko@gmail.com>
2024-09-02 12:49:58 +0300

    Quest 2 fix #4

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,7 @@
-printf "Chameleon\n28\nMale"
+#!/bin/bash
+
+# Download the JSON file
+curl -o superheroes.json "https://platform.zone01.gr/assets/superhero/all.json"
+
+# Extract and print Name, Power, and Gender of id 170
+jq -r '.[] | select(.id == 170) | "\(.name)\n\(.powerstats.power)\n\(.appearance.gender)"' superheroes.json
