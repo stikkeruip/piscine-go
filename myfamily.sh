5d26ffd2342e2b0f24aa03d9384549f17e9a40cc
ustikker <stikkeruipko@gmail.com>
2024-09-02 15:42:06 +0300

    gf

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,3 @@
 curl -s "https://platform.zone01.gr/assets/superhero/all.json" | \
-jq -r --arg hero_id "$HERO_ID" '.[] | select(.id == $HERO_ID) | "\(.name)\n\(.powerstats.power)\n\(.appearance.gender)"'
+jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID | tonumber)) | .connections.relatives'
+
