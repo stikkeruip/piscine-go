e612d6e7b0a552a5099a38047463611729942350
uipko <stikkeruipko@gmail.com>
2024-09-04 15:26:44 +0300

    ghfd

--- printnbr.go
+++ printnbr.go
@@ -5,6 +5,11 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
+		if n == -9223372036854775808 {
+			PrintNbr(-(n / 10))
+			z01.PrintRune('8')
+			return
+		}
 		n = -n
 	}
 	if n >= 10 {
