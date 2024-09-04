4b71b0931087f69a332b18dbae2d8844d9cda1ae
uipko <stikkeruipko@gmail.com>
2024-09-04 14:54:24 +0300

    gfd

--- printcomb2.go
+++ printcomb2.go
@@ -1,25 +1,21 @@
-package printcomb2
+package main
 
 import "github.com/01-edu/z01"
 
-func PrintComb2() {
+func main() {
 	first := true
-	for a := 0; a < 10; a++ {
-		for b := 0; b < 10; b++ {
-			for c := 0; c < 10; c++ {
-				for d := 0; d < 10; d++ {
-					if !first {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
-					z01.PrintRune(rune(a + '0'))
-					z01.PrintRune(rune(b + '0'))
-					z01.PrintRune(' ')
-					z01.PrintRune(rune(c + '0'))
-					z01.PrintRune(rune(d + '0'))
-					first = false
-				}
+	for a := 0; a <= 99; a++ {
+		for b := a + 1; b <= 99; b++ {
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
+			z01.PrintRune(rune(a/10 + '0'))
+			z01.PrintRune(rune(a%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(b/10 + '0'))
+			z01.PrintRune(rune(b%10 + '0'))
+			first = false
 		}
 	}
 }
