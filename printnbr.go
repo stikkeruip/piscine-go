356f9e4b82c9904842cd6f3cf6f188ea11c818e9
uipko <stikkeruipko@gmail.com>
2024-09-04 15:18:10 +0300

    ghfd

---
+++ printnbr.go
@@ -0,0 +1,14 @@
+package printcomb
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n >= 10 {
+		PrintNbr(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
