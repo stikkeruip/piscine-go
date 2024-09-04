b10fa0cc7e139eabd2d1b35a60e53d637dabbaeb
uipko <stikkeruipko@gmail.com>
2024-09-04 13:49:21 +0300

    gfd

--- isnegative.go
+++ isnegative.go
@@ -3,11 +3,11 @@ package isnegative
 import "github.com/01-edu/z01"
 
 func isnegative(nb int) {
-	if(nb >= 0) {
+	if nb >= 0 {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	}
-	if(nb < 0) {
+	if nb < 0 {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	}
