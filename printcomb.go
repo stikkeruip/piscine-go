e59ea178523115f692737dd4a846a0e7b339a5b2
uipko <stikkeruipko@gmail.com>
2024-09-04 14:10:46 +0300

    gfd

--- printcomb.go
+++ printcomb.go
@@ -3,18 +3,18 @@ package printcomb
 import "github.com/01-edu/z01 v0.1.0"
 
 func PrintComb() {
-		first := true 
-		for a := 0; a < 10; a++ {
-			for b := a + 1; b < 10; b++ {
-				for c := b + 1; c < 10; c++ {
-					if !first {
-						z01.PrintRune(", ")
-					}
-					z01.PrintRune(a)
-					z01.PrintComb(b)
-					z01.PrintComb(c)
-					first = false;
+	first := true
+	for a := 0; a < 10; a++ {
+		for b := a + 1; b < 10; b++ {
+			for c := b + 1; c < 10; c++ {
+				if !first {
+					z01.PrintRune(", ")
 				}
+				z01.PrintRune(a)
+				z01.PrintComb(b)
+				z01.PrintComb(c)
+				first = false
 			}
 		}
+	}
 }
