2fc9d5931ea78e4911f6ebef351c3aba9370504f
ustikker <stikkeruipko@gmail.com>
2024-09-05 17:20:52 +0300

    gdf

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func BasicAtoi2(s string) int {
 	rns := []rune(s)
@@ -14,11 +12,4 @@ func BasicAtoi2(s string) int {
 		}
 	}
 	return result
-}
-
-func main() {
-	fmt.Println(BasicAtoi2("12345"))
-	fmt.Println(BasicAtoi2("0000000012345"))
-	fmt.Println(BasicAtoi2("012 345"))
-	fmt.Println(BasicAtoi2("Hello World!"))
 }
