3ff41d890a9103410d976c16d0eef732993e33ae
ustikker <stikkeruipko@gmail.com>
2024-09-09 14:46:12 +0300

    gf

---
+++ fibonacci.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Fibonacci(n int) int {
+	if n < 0 {
+		return -1
+	}
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+
+	return (Fibonacci(n-1) + Fibonacci(n-2))
+}
