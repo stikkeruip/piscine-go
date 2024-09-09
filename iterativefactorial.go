e0117261eddfc9157876d90c7082fbb556b7b016
ustikker <stikkeruipko@gmail.com>
2024-09-09 13:32:37 +0300

    fg

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,17 +4,13 @@ func IterativeFactorial(n int) int {
 	if n == 0 {
 		return 1
 	}
-	if n < 0 {
+	if n < 0 || n >= 21 {
 		return 0
 	}
 
 	fac := 1
 	for i := 1; i <= n; i++ {
 		fac = fac * i
-
-		if fac/i != fac/i {
-			return 0
-		}
 	}
 
 	return fac
