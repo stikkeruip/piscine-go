70cbd2978cb2ecfc96f3b2fc9ea5d0d92831d28a
ustikker <stikkeruipko@gmail.com>
2024-09-10 13:39:37 +0300

    gf

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,12 @@
 package piscine
 
 func AlphaCount(s string) int {
-	runes := []string(s)
+	runes := []rune(s)
 
 	count := 0
 
-	for _, i ; range runes {
-		if (i >= 'a' && i <= 'Z') {
+	for _, i := range runes {
+		if i >= 'a' && i <= 'Z' {
 			count++
 		}
 	}
