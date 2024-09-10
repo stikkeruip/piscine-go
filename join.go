aa634a8980513afb8736e302a1488bf0c8f20d0c
ustikker <stikkeruipko@gmail.com>
2024-09-10 15:31:19 +0300

    gd

--- join.go
+++ join.go
@@ -3,8 +3,14 @@ package piscine
 func Join(strs []string, sep string) string {
 	s := ""
 
-	for _, i := range strs {
-		s = s + i + sep
+	for v, i := range strs {
+		s = s + i
+
+		if v == len(strs)-1 {
+			return s
+		}
+
+		s = s + sep
 	}
 
 	return s
