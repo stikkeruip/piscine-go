44ac5034c91bc93acb8486e98eb796a02d6e2bdd
ustikker <stikkeruipko@gmail.com>
2024-09-11 16:44:12 +0300

    fgd

---
+++ printprogramname/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	file := getBaseFileName(os.Args[0])
+
+	for _, i := range file {
+		z01.PrintRune(i)
+	}
+}
+
+func getBaseFileName(path string) string {
+	for i := len(path) - 1; i > 0; i-- {
+		if path[i] == '/' {
+			return path[i+1:]
+		}
+	}
+	return path
+}
