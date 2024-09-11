1c7fb697880f33812f49c56c7f35dfe48de40b92
ustikker <stikkeruipko@gmail.com>
2024-09-11 17:00:47 +0300

    yuh

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -16,7 +16,7 @@ func main() {
 
 func getBaseFileName(path string) string {
 	for i := len(path) - 1; i > 0; i-- {
-		if path[i] == '/' {
+		if path[i] == '/' || path[i] == '\\' {
 			return path[i+1:]
 		}
 	}
