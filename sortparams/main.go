a493bd7a90ee1b1cad7d774b5e2209246c0879de
ustikker <stikkeruipko@gmail.com>
2024-09-11 18:23:05 +0300

    yuh

--- sortparams/main.go
+++ sortparams/main.go
@@ -7,21 +7,24 @@ import (
 )
 
 func main() {
-	var runes []rune
+	var str []string
 
 	for i := 1; i < len(os.Args); i++ {
-		runes = append(runes, []rune(os.Args[i])[0])
+		str = append(str, os.Args[i])
 	}
 
-	for i := 0; i < len(runes)-1; i++ {
-		for j := 0; j < len(runes)-i-1; j++ {
-			if runes[j] > runes[j+1] {
-				runes[j], runes[j+1] = runes[j+1], runes[j]
+	for i := 0; i < len(str)-1; i++ {
+		for j := 0; j < len(str)-i-1; j++ {
+			if str[j] > str[j+1] {
+				str[j], str[j+1] = str[j+1], str[j]
 			}
 		}
 	}
 
-	for _, i := range runes {
-		z01.PrintRune(i)
+	for _, i := range str {
+		for _, j := range i {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune('\n')
 	}
 }
