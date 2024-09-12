190382827d5cb0e5aacde67a28d1b8355a2cf332
uipko <stikkeruipko@gmail.com>
2024-09-12 10:14:43 +0300

    yes

---
+++ rotatevowels/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		z01.PrintRune('\n')
+		return
+	}
+
+	str := ""
+	for i := 1; i < len(os.Args); i++ {
+		str += os.Args[i] + " "
+	}
+
+	runes := []rune(str)
+	left, right := 0, len(runes)-1
+
+	for left < right {
+		for left < right && !containsVowel(runes[left]) {
+			left++
+		}
+		for left < right && !containsVowel(runes[right]) {
+			right--
+		}
+		if left < right {
+			runes[left], runes[right] = runes[right], runes[left]
+			left++
+			right--
+		}
+	}
+
+	for i := 0; i < len(runes)-1; i++ {
+		z01.PrintRune(runes[i])
+	}
+}
+
+func containsVowel(r rune) bool {
+	vowels := "aeiouAEIOU"
+	for _, v := range vowels {
+		if r == v {
+			return true
+		}
+	}
+	return false
+}
