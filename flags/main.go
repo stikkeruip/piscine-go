dd3672532919cc995bf0e8dc643b9cd423cc092b
uipko <stikkeruipko@gmail.com>
2024-09-12 09:12:05 +0300

    yes

--- flags/main.go
+++ flags/main.go
@@ -66,8 +66,8 @@ func orderString(str string) string {
 func printHelp() {
 	fmt.Println("--insert")
 	fmt.Println("  -i")
-	fmt.Println("	This flag inserts the string into the string passed as argument.")
+	fmt.Println("	 This flag inserts the string into the string passed as argument.")
 	fmt.Println("--order")
 	fmt.Println("  -o")
-	fmt.Println("	This flag will behave like a boolean, if it is called it will order the argument.")
+	fmt.Println("	 This flag will behave like a boolean, if it is called it will order the argument.")
 }
