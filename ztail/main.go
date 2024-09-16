38c4a4c8a621e3cbebf1071604465ccba32a4ca8
ustikker <stikkeruipko@gmail.com>
2024-09-16 17:33:48 +0300

    yuh

--- ztail/main.go
+++ ztail/main.go
@@ -16,6 +16,7 @@ func checkFile(e error, name string) bool {
 func main() {
 	numStr := os.Args[2]
 	num := 0
+	errExit := false
 
 	for _, r := range numStr {
 		num = num*10 + int(r-'0')
@@ -30,7 +31,11 @@ func main() {
 			fmt.Printf("==> %s <==\n", f)
 			fmt.Printf("%s\n", data[size-int64(num):])
 		} else {
-			os.Exit(1)
+			errExit = true
 		}
+	}
+
+	if errExit {
+		os.Exit(1)
 	}
 }
