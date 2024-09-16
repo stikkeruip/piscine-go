894c0039da42f6bca2b36a938eda7a573e22d3ce
ustikker <stikkeruipko@gmail.com>
2024-09-16 13:27:11 +0300

    yuh

---
+++ cat/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func checkFile(e error, name string) {
+	if e != nil {
+		fmt.Printf("ERROR: open %s: No such file or directory", name)
+	}
+}
+
+func main() {
+	//filenames := os.Args[1:]
+
+	for _, f := range os.Args[1:] {
+		data, err := os.ReadFile(f)
+		checkFile(err, f)
+		fmt.Println(data)
+	}
+}
