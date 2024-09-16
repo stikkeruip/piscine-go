e1649dc5f9c418c01e84ca1615b3ade082e8cf1f
ustikker <stikkeruipko@gmail.com>
2024-09-16 17:48:06 +0300

    yuh

--- ztail/main.go
+++ ztail/main.go
@@ -32,7 +32,7 @@ func main() {
 				start = 0
 			}
 			data, _ := os.ReadFile(f)
-			fmt.Printf("==> %s <==\n", f)
+			fmt.Printf("\n==> %s <==", f)
 			fmt.Printf("%s", data[start:])
 		} else {
 			fmt.Printf("\n")
