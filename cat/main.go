05b250dc144e4cad8d3eacdccc6005b6f2ae01f6
ustikker <stikkeruipko@gmail.com>
2024-09-16 14:08:39 +0300

    yuh

--- cat/main.go
+++ cat/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
-func checkFile(e error, name string) {
+func checkFile(e error, name string) bool {
 	if e != nil {
-		fmt.Printf("ERROR: open %s: No such file or directory", name)
+		str := "ERROR: open " + name + ": No such file or directory"
+		for _, r := range str {
+			z01.PrintRune(r)
+		}
+
+		z01.PrintRune('\n')
+		return false
 	}
+	return true
 }
 
 func main() {
 	// filenames := os.Args[1:]
+	if len(os.Args) == 1 {
+		reader := bufio.NewReader(os.Stdin)
+		message, _ := reader.ReadString('\n')
 
+		for _, r := range message {
+			z01.PrintRune(r)
+		}
+		return
+	}
 	for _, f := range os.Args[1:] {
 		data, err := os.ReadFile(f)
-		checkFile(err, f)
-		fmt.Println(data)
+		if checkFile(err, f) {
+			fmt.Println(data)
+		}
 	}
 }
