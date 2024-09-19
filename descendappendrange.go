ad8c9e908ef2aa86866958436c778c382622ac1f
uipko <stikkeruipko@gmail.com>
2024-09-19 23:15:16 +0300

    yes

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,14 +1,10 @@
 package piscine
 
 func DescendAppendRange(max, min int) []int {
-	if max < min {
+	if max <= min {
 		return []int{}
 	}
-
 	var numSlice []int
-	if max == min {
-		numSlice = append(numSlice, max)
-	}
 
 	for i := max; i > min; i-- {
 		numSlice = append(numSlice, i)
