6543417ba391b2358d36da0af25ed365dc9d6e4e
ustikker <stikkeruipko@gmail.com>
2024-09-19 11:11:22 +0300

    yuh

---
+++ descendcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+	first := true
+	for a := 99; a >= 0; a-- {
+		for b := a - 1; b >= 00; b-- {
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune(rune(a/10 + '0'))
+			z01.PrintRune(rune(a%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(b/10 + '0'))
+			z01.PrintRune(rune(b%10 + '0'))
+			first = false
+		}
+	}
+	z01.PrintRune('\n')
+}
