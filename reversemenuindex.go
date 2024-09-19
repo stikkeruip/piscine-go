da18bbdfb993dd554d729664fa58a7bff037dc84
uipko <stikkeruipko@gmail.com>
2024-09-20 02:26:51 +0300

    yes

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -2,8 +2,9 @@ package piscine
 
 func ReverseMenuIndex(menu []string) []string {
 	n := len(menu)
-	for i := 0; i < n/2; i++ {
-		menu[i], menu[n-1-i] = menu[n-1-i], menu[i]
+	reversed := make([]string, n)
+	for i := 0; i < n; i++ {
+		reversed[i] = menu[n-1-i]
 	}
-	return menu
+	return reversed
 }
