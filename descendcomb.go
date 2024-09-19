d846b68932a3810ce3dc3fce7622438ffb08ca49
ustikker <stikkeruipko@gmail.com>
2024-09-19 11:18:37 +0300

    yuh

--- descendcomb.go
+++ descendcomb.go
@@ -3,19 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-	first := true
 	for a := 99; a > 0; a-- {
 		for b := a - 1; b >= 0; b-- {
-			if !first {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+			if a == 01 && b == 00 {
+				z01.PrintRune(rune(a/10 + '0'))
+				z01.PrintRune(rune(a%10 + '0'))
+				break
 			}
 			z01.PrintRune(rune(a/10 + '0'))
 			z01.PrintRune(rune(a%10 + '0'))
 			z01.PrintRune(' ')
 			z01.PrintRune(rune(b/10 + '0'))
 			z01.PrintRune(rune(b%10 + '0'))
-			first = false
+
+			if !(a == 1 && b == 0) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
 		}
 	}
 	z01.PrintRune('\n')
