f705f3fd8b94f014cbe2605f3bfc07d567955e67
ustikker <stikkeruipko@gmail.com>
2024-09-19 10:25:10 +0300

    yuh

--- rot14.go
+++ rot14.go
@@ -1,11 +1,15 @@
 package piscine
 
 func Rot14(s string) string {
-	runes := []rune(s)
+	runes := []rune(s) // Convert the input string to a slice of runes.
 
-	for _, r := range s {
-		runes = append(runes, r+14)
+	for i, r := range runes {
+		if r >= 'a' && r <= 'z' {
+			runes[i] = 'a' + (r-'a'+14)%26 // Rotate within lowercase letters.
+		} else if r >= 'A' && r <= 'Z' {
+			runes[i] = 'A' + (r-'A'+14)%26 // Rotate within uppercase letters.
+		}
 	}
 
-	return string(runes)
+	return string(runes) // Convert the modified runes back to a string.
 }
