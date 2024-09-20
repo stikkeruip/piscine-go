e33beada3dd0db9527a28eccb6075450513b2a6d
ustikker <stikkeruipko@gmail.com>
2024-09-20 09:24:25 +0300

    yuh

---
+++ loafofbread.go
@@ -0,0 +1,22 @@
+package piscine
+
+func LoafOfBread(str string) string {
+	if len(str) < 5 {
+		return "Invalid Input\n"
+	}
+	finStr := ""
+	count := 0
+	for _, r := range str {
+		if count == 5 {
+			finStr += " "
+			count = 0
+			continue
+		}
+		if r == ' ' {
+			continue
+		}
+		finStr += string(r)
+		count++
+	}
+	return finStr + "\n"
+}
