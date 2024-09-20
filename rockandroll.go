30bd41bd43ac6256fb6e02a77fe66a8ab03327e1
ustikker <stikkeruipko@gmail.com>
2024-09-20 08:13:00 +0300

    yuh

--- rockandroll.go
+++ rockandroll.go
@@ -4,9 +4,6 @@ func RockAndRoll(n int) string {
 	if n < 0 {
 		return "error: number is negative\n"
 	}
-	if n == 0 {
-		return "error: non divisible\n"
-	}
 	if n%2 == 0 && n%3 == 0 {
 		return "rock and roll\n"
 	}
@@ -16,5 +13,5 @@ func RockAndRoll(n int) string {
 	if n%3 == 0 {
 		return "roll\n"
 	}
-	return ""
+	return "error: non divisible\n"
 }
