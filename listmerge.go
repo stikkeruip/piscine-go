18dc10e587dc250399172f54807d89f6bb56c319
uipko <stikkeruipko@gmail.com>
2024-09-24 01:05:29 +0300

    yes

--- listmerge.go
+++ listmerge.go
@@ -1,15 +1,6 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	curr1 := l1.Tail
-	curr2 := l2.Head
-
-	for curr2 != nil {
-		curr1.Next = curr2
-		if curr1.Next != nil {
-			curr1 = curr1.Next
-		}
-		curr2 = curr2.Next
-	}
-	l1.Tail = curr1
+	l1.Tail.Next = l2.Head
+	l1.Tail = l2.Tail
 }
