9c3ea83b920aef07869468fb3aef83c3f043cdda
uipko <stikkeruipko@gmail.com>
2024-09-23 16:10:39 +0300

    yuh

--- listreverse.go
+++ listreverse.go
@@ -1,5 +1,13 @@
 package piscine
 
 func ListReverse(l *List) {
+	curr := l.Head
+	var prev *NodeL
 
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
 }
