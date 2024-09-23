efedb03e1339f914a472e500eabfa8c6a7b5ec04
uipko <stikkeruipko@gmail.com>
2024-09-24 01:02:33 +0300

    yes

--- listmerge.go
+++ listmerge.go
@@ -6,7 +6,9 @@ func ListMerge(l1 *List, l2 *List) {
 
 	for curr2 != nil {
 		curr1.Next = curr2
-		curr1 = curr1.Next
+		if curr1.Next != nil {
+			curr1 = curr1.Next
+		}
 		curr2 = curr2.Next
 	}
 	l1.Tail = curr1
