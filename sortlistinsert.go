b3625f7bc268c4dc0e75d3696812d74cdd3d41e7
ustikker <stikkeruipko@gmail.com>
2024-09-24 15:08:06 +0300

    yuh

---
+++ sortlistinsert.go
@@ -0,0 +1,23 @@
+package piscine
+
+func SortListInsert(l *NodeI, data_ref int) *NodeI {
+	curr := l
+	temp := &NodeI{Data: data_ref}
+	prev := temp
+
+	if l.Data > data_ref {
+		temp.Next = l
+		return temp
+	}
+
+	for curr != nil {
+		if curr.Data > data_ref {
+			prev.Next = temp
+			temp.Next = curr
+			return l
+		}
+		prev = curr
+		curr = curr.Next
+	}
+	return l
+}
