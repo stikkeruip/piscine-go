bc0a9c2e7ab1ff553f5ddfca2092200a0aa5f732
ustikker <stikkeruipko@gmail.com>
2024-09-24 17:21:21 +0300

    yuh

---
+++ btreeapplyinorder.go
@@ -0,0 +1,13 @@
+package piscine
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	BTreeApplyInorder(root.Left, f)
+
+	f(root)
+
+	BTreeApplyInorder(root.Right, f)
+}
