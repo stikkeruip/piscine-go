a645a0e935e741f125f3c54ac54748429bd03c65
ustikker <stikkeruipko@gmail.com>
2024-09-25 14:22:02 +0300

    yuh

---
+++ btreeapplybylevel.go
@@ -0,0 +1,11 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	f(root.Data)
+	BTreeApplyByLevel(root.Left, f)
+	BTreeApplyByLevel(root.Right, f)
+}
