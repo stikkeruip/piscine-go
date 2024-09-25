0ed87dfb66740f7f092f2917fe7f9a8ead0f526a
ustikker <stikkeruipko@gmail.com>
2024-09-25 15:33:16 +0300

    yuh

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,7 +1,9 @@
 package piscine
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	node = rplc
+	node.Data = rplc.Data
+	node.Left = rplc.Left
+	node.Right = rplc.Right
 
 	return root
 }
